44931b35d4c2c07596ee2a1dc1d70de5bff60e9a
CitaZane <zanekrumina2@gmail.com>
2022-12-01 08:26:16 +0200

    first-day solve

--- day_01/main.go
+++ day_01/main.go
@@ -2,25 +2,53 @@ package main
 
 import (
 	"adventofcode2022/utils"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-func run(input string) (interface{}, interface{}) {
-	nums := utils.ParseIntList(input, "\n")
-	solve1 := firstMesurement(nums)
-
-	return solve1, nil
-}
-
-func firstMesurement(data []int) int {
-	count := 0
-	for i := 0; i < len(data)-1; i++ {
-		if data[i+1] > data[i] {
-			count++
-		}
-	}
-	return count
-}
+// --- Day 1: Calorie Counting ---
 
 func main() {
 	utils.Run(run, puzzle)
+}
+
+func run(input string) (interface{}, interface{}) {
+	caloriesTotal := strings.Split(input, "\n")
+	sortedCalorieList := readAndCount(caloriesTotal)
+
+	solve1 := calcMaxCalories(sortedCalorieList)
+	solve2 := calcTopThreeCalories(sortedCalorieList)
+
+	return solve1, solve2
+}
+
+func readAndCount(caloriesTotal []string) []int {
+	caloriesPerElf := []int{}
+	accumulator := 0
+	for _, line := range caloriesTotal {
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			caloriesPerElf = append(caloriesPerElf, accumulator)
+			accumulator = 0
+		} else {
+			accumulator += number
+		}
+	}
+	sort.Ints(caloriesPerElf)
+	return caloriesPerElf
+}
+
+func calcTopThreeCalories(list []int) int {
+	index := len(list) - 1
+	total := 0
+	for i := index; i > index-3; i-- {
+		total += list[i]
+	}
+	return total
+}
+
+func calcMaxCalories(list []int) int {
+	index := len(list) - 1
+	return list[index]
 }
